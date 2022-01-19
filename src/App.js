3d1469132d02fc73e163619cffb6c600babca5fe
Azonkeu <mazonkeu@gmail.com>
2022-01-19 23:50:48 +0100

    Remove images and text

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
+import React from 'react';
 import Calculate from './components/Calculator';
 
-function App() {
-  return (
-    <div className="App">
-      <Calculate />
-    </div>
-  );
+// eslint-disable-next-line react/prefer-stateless-function
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Calculate />
+      </div>
+    );
+  }
 }
 
 export default App;
